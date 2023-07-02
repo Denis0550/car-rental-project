18e511b7fe33464c0840b587da0682579065b105
Denis <denis.soroka5@pm.me>
2023-07-02 11:26:25 +0300

    frontend client interface created

---
+++ frontend/src/app/models/client.ts
@@ -0,0 +1,14 @@
+export interface Client {
+
+  id: number;
+  name: string | null;
+  surname: string | null;
+  phone: string;
+  email: string;
+  address: string | null;
+  hasDrivingLicense: boolean;
+  registrationDateTime: string;
+  dateOfBirth: string
+
+
+}
