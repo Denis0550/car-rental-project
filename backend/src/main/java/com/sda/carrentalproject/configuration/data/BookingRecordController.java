a413eae4fb79d2fb40b66c5ddd89bd670f0b707f
Denis <denis.soroka5@pm.me>
2023-07-15 11:49:02 +0300

    endpoint for all booking records created

--- backend/src/main/java/com/sda/carrentalproject/configuration/data/BookingRecordController.java
+++ backend/src/main/java/com/sda/carrentalproject/configuration/data/BookingRecordController.java
@@ -1,8 +1,8 @@
 package com.sda.carrentalproject.configuration.data;
 
 import com.sda.carrentalproject.dto.BookingRecordDto;
+import com.sda.carrentalproject.mapper.BookingRecordMapper;
 import com.sda.carrentalproject.service.BookingRecordService;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
@@ -16,14 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookingRecordController {
 
   private final BookingRecordService bookingRecordService;
+  private final BookingRecordMapper bookingRecordMapper;
 
-  public BookingRecordController(BookingRecordService bookingRecordService) {
+  public BookingRecordController(BookingRecordService bookingRecordService,
+      BookingRecordMapper bookingRecordMapper) {
     this.bookingRecordService = bookingRecordService;
+    this.bookingRecordMapper = bookingRecordMapper;
   }
 
   @GetMapping("/booking-records")
   public List<BookingRecordDto> allBookingRecords() {
-    return Collections.emptyList();
+    log.info("getting all booking records");
+
+    return bookingRecordService.findAllBookingRecords()
+        .stream()
+        .map(bookingRecord -> bookingRecordMapper.fromEntityToDto(bookingRecord))
+        .toList();
   }
 
 }
