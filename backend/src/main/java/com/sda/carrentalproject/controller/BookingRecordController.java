47111ba4b3b1df654b67fc7087f534bfd70a0350
Denis <denis.soroka5@pm.me>
2023-07-16 10:59:39 +0300

    BookingService - tests for calculate Booking price method improved

--- backend/src/main/java/com/sda/carrentalproject/controller/BookingRecordController.java
+++ backend/src/main/java/com/sda/carrentalproject/controller/BookingRecordController.java
@@ -1,18 +1,23 @@
-package com.sda.carrentalproject.configuration.data;
+package com.sda.carrentalproject.controller;
 
+import com.sda.carrentalproject.domain.BookingRecord;
 import com.sda.carrentalproject.dto.BookingRecordDto;
 import com.sda.carrentalproject.dto.CarBookingRequestDto;
 import com.sda.carrentalproject.mapper.BookingRecordMapper;
 import com.sda.carrentalproject.service.BookingRecordService;
+import java.net.URI;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @Slf4j
@@ -39,11 +44,16 @@ public class BookingRecordController {
   }
 
   @PostMapping("/booking-records")
-  public BookingRecordDto bookCar(@RequestBody CarBookingRequestDto carBookingRequest) {
+  public ResponseEntity<BookingRecordDto> bookCar(
+      @RequestBody CarBookingRequestDto carBookingRequest,
+      UriComponentsBuilder ucb) {
+    log.info("trying to book car with arguments: [{}]", carBookingRequest);
+    BookingRecord saved = bookingRecordService.createNewBooking(carBookingRequest);
+    URI path = ucb.path("/api/booking-records/{id}")
+        .buildAndExpand(Map.of("id", saved.getId()))
+        .toUri();
+    return ResponseEntity.created(path)
+        .body(bookingRecordMapper.fromEntityToDto(saved));
 
-
-
-    return null;
   }
-
 }
